9c66d7f605b2ca37d37a6fab94e03e224114060d
Chris Jenkins <chris.mark.jenkins@gmail.com>
2024-09-17 00:19:52 -0600

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,39 @@
+rootProject.name = "sqkon"
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven {
+           setUrl("https://jitpack.io")
+           content {
+                includeGroupByRegex("com\\.github.*")
+           }
+        }
+        google {
+            content {
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+//includeBuild("convention-plugins")
+include(":library")
+
