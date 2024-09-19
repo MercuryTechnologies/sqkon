c4aa367b66d2411251422b6d99efad03e5c5857e
Chris Jenkins <chris.mark.jenkins@gmail.com>
2024-09-19 11:39:01 -0600

    added descriptor test

---
+++ library/src/commonTest/kotlin/com/mercury/sqkon/DescriptorTest.kt
@@ -0,0 +1,32 @@
+package com.mercury.sqkon
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.StructureKind
+import kotlinx.serialization.descriptors.elementDescriptors
+import kotlinx.serialization.serializer
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+@OptIn(ExperimentalSerializationApi::class)
+class DescriptorTest {
+
+    @Test
+    fun getDescriptorInfoOfInlineClass() {
+        val d = getDescriptor<TestValue>()
+        assertTrue(d.isInline)
+    }
+
+    @Test
+    fun getDescriptorInfoOfLis() {
+        val d = getDescriptor<List<TestObjectChild>>()
+        assertEquals(StructureKind.LIST, d.kind)
+        println(d.elementDescriptors.forEach { println(it) })
+    }
+
+    private inline fun <reified T : Any> getDescriptor(): SerialDescriptor {
+        return serializer<T>().descriptor
+    }
+
+}
