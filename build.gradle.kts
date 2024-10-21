1227b530c7eec843b76f2cf71fe5180e9c566280
Chris Jenkins <chris.mark.jenkins@gmail.com>
2024-10-21 16:18:02 -0600

    apply false

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.android.library).apply(false)
     alias(libs.plugins.kotlinx.serialization).apply(false)
     alias(libs.plugins.sqlDelight).apply(false)
-    alias(libs.plugins.maven.publish)
+    alias(libs.plugins.maven.publish).apply(false)
 }
 
 allprojects {
