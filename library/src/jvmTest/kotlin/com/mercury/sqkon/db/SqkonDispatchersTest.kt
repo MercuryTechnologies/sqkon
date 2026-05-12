07a80476afe4f2633eb0a0e8c03d9b9cadc08e4c
Chris Jenkins <chris.mark.jenkins@gmail.com>
2026-05-12 13:38:54 -0600

    test: drop tautological dispatcher reachability test

--- library/src/jvmTest/kotlin/com/mercury/sqkon/db/SqkonDispatchersTest.kt
+++ library/src/jvmTest/kotlin/com/mercury/sqkon/db/SqkonDispatchersTest.kt
@@ -1,29 +1,14 @@
 package com.mercury.sqkon.db
 
 import com.mercury.sqkon.TestObject
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
-import kotlin.test.AfterTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertNotSame
 
 class SqkonDispatchersTest {
-
-    private val mainScope = MainScope()
-
-    @AfterTest
-    fun tearDown() { mainScope.cancel() }
-
-    @Test
-    fun defaultDispatchers_areReachable() {
-        val d = defaultSqkonDispatchers
-        assertNotSame(d.read, d.write)
-    }
 
     @Test
     fun customDispatchers_areThreadedThroughSqkonConstructor() = runTest {
