86e7f1dec235d5f59b4a23ee33089e730020a6bd
Chris Jenkins <chris.mark.jenkins@gmail.com>
2026-05-19 16:12:06 -0600

    feat(arch): SqkonCursor + SqlDelight bridge (MOB-3289)

---
+++ library/src/commonMain/kotlin/com/mercury/sqkon/db/internal/SqkonCursor.kt
@@ -0,0 +1,10 @@
+package com.mercury.sqkon.db.internal
+
+internal interface SqkonCursor {
+    fun next(): Boolean
+    fun getString(index: Int): String?
+    fun getLong(index: Int): Long?
+    fun getBytes(index: Int): ByteArray?
+    fun getDouble(index: Int): Double?
+    fun getBoolean(index: Int): Boolean?
+}
